4acbf3218076d31a00c30cd71d80747fead70ced
eunhye0619 <eunhye3814@gmail.com>
2023-11-13 16:36:31 +0900

    파일

--- index.py
+++ index.py
@@ -1,20 +1,30 @@
+import time
 from flask import Flask, request, render_template, jsonify
 import RPi.GPIO as gpio
-from time
+import MariaDB
+
+app = Flask(__name__)
+store = MariaDB.MariaDB()
+
 
 pin1 = 16
 pin2 = 18
 
-def water()
-
 gpio.setmode(gpio.BOARD)
-gpio.setup(pin1, gpio.OUT)
 gpio.setup(pin2, gpio.IN)
+gpio.setup(pin1, gpio.OUT)
 servo = gpio.PWM(pin1, 50)
+
+
+def water():
+    level_data = gpio.input(pin2)
+    print(level_data)
+    time.sleep(0.5)
+
 
 @app.route("/")
 def home():
-    return render_template("")
+    return render_template("index.html")
 
 
 def setAngle(angle):
@@ -23,7 +33,7 @@ def setAngle(angle):
     servo.ChangeDutyCycle(duty)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     servo.start(0)
     app.run(host="0.0.0.0")
     servo.stop()
